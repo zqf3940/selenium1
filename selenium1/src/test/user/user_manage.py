352badb971d954c03bf8475e90d06f8f90efc11c
zqf39402 <zqf3940@163.com>
2018-01-30 19:14:48 +0800

    1

--- selenium1/src/test/user/user_manage.py
+++ selenium1/src/test/user/user_manage.py
@@ -9,6 +9,7 @@ class user_manage(base_login):
 
     driver = webdriver.Chrome()
     base_login.getLoginIndex(driver)
+    #隐形等待,防止未加载完成
     driver.implicitly_wait(5)
     menu_user = driver.find_element_by_xpath('//ul[@id="mainnav-menu"]/li[2]')
-    menu_user.click()
+    menu_user.click()#打开用户管理菜单
